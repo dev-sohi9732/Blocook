be30ba228169c5ca58a8cb17230fa42fb370745f
김소희 <sohi9732@naver.com>
2020-08-05 09:48:05 +0900

    S03P13A505-9 [sohee] : fix addrecipe ingrnt input box

--- blocook_vue/src/views/recipe/Addrecipe.vue
+++ blocook_vue/src/views/recipe/Addrecipe.vue
@@ -8,7 +8,7 @@
             <center class="menu">
               <img src="@/assets/img/addimage.jpg">
               <div>
-                <button class="button btn" style="width:180px;">대표 이미지 등록&nbsp;<i class="fa fa-camera"></i></button>
+                <button class="button btn" style="width:190px;">대표 이미지 등록&nbsp;<i class="fa fa-camera"></i></button>
               </div>
             </center>
 
@@ -19,7 +19,7 @@
                   <th>
                     <h6>레시피명</h6>
                   </th>
-                  <td>
+                  <td colspan="3">
                     <b-form-input v-model="recipenm" size="sm" placeholder="레시피명 입력"></b-form-input>
                   </td>
                 </tr>
@@ -28,10 +28,25 @@
                     <h6>주재료</h6>
                   </th>
                   <td>
-                    <b-form-input v-model="mainirdnt" list="my-list-id" size="sm" placeholder="필수재료 입력"></b-form-input>
+                    <b-form-input v-model="mainirdnt" list="my-list-id" size="sm" placeholder="필수재료"></b-form-input>
                     <datalist id="my-list-id">
                       <option v-for="(irdnt, index) in irdnts" :key="index">{{ irdnt }}</option>
                     </datalist>
+                  </td>
+                  <td style="width:25%;">
+                    <b-form-input v-model="mainirdnt_amount" size="sm" placeholder="양(단위)"></b-form-input>
+                  </td>
+                  <td>
+                    <button class="btn" @click="addMainIrdnt()"><i class="fa fa-plus" style="color:blue;"></i></button>
+                  </td>
+                </tr>
+                <tr v-for="(mainirdnt, index) in arr_mainirdnt" :key="index">
+                  <td></td>
+                  <td colspan="3">
+                    <p style="margin-bottom:8px;">
+                      <i class="fa fa-minus" @click="rmMainIrdnt(index)" style="color:red;"></i>
+                      &nbsp;{{ mainirdnt.mainirdnt_nm }}&nbsp;{{ mainirdnt.mainirdnt_vol }}
+                    </p>
                   </td>
                 </tr>
                 <tr>
@@ -39,10 +54,25 @@
                     <h6>부재료</h6>
                   </th>
                   <td>
-                    <b-form-input v-model="subirdnt" list="my-list-id" size="sm" placeholder="선택재료 입력"></b-form-input>
+                    <b-form-input v-model="subirdnt" list="my-list-id" size="sm" placeholder="선택재료"></b-form-input>
                     <datalist id="my-list-id">
                       <option v-for="(irdnt, index) in irdnts" :key="index">{{ irdnt }}</option>
                     </datalist>
+                  </td>
+                  <td>
+                    <b-form-input v-model="subirdnt_amount" size="sm" placeholder="양(단위)"></b-form-input>
+                  </td>
+                  <td>
+                    <button class="btn" @click="addSubIrdnt()"><i class="fa fa-plus" style="color:blue;"></i></button>
+                  </td>
+                </tr>
+                <tr v-for="(subirdnt, index) in arr_subirdnt" :key="index">
+                  <td></td>
+                  <td colspan="3">
+                    <p style="margin-bottom:8px;">
+                      <i class="fa fa-minus" @click="rmSubIrdnt(index)" style="color:red;"></i>
+                      &nbsp;{{ subirdnt.subirdnt_nm }}&nbsp;{{ subirdnt.subirdnt_vol }}
+                    </p>
                   </td>
                 </tr>
                 <tr>
@@ -50,10 +80,25 @@
                     <h6>양념</h6>
                   </th>
                   <td>
-                    <b-form-input v-model="seasoningirdnt" list="my-list-id" size="sm" placeholder="선택재료 입력"></b-form-input>
+                    <b-form-input v-model="seasoningirdnt" list="my-list-id" size="sm" placeholder="양념"></b-form-input>
                     <datalist id="my-list-id">
                       <option v-for="(irdnt, index) in irdnts" :key="index">{{ irdnt }}</option>
                     </datalist>
+                  </td>
+                  <td>
+                    <b-form-input v-model="seasoningirdnt_amount" size="sm" placeholder="양(단위)"></b-form-input>
+                  </td>
+                  <td>
+                    <button class="btn" @click="addSeasoningIrdnt()"><i class="fa fa-plus" style="color:blue;"></i></button>
+                  </td>
+                </tr>
+                <tr v-for="(seasoningirdnt, index) in arr_seasoningirdnt" :key="index">
+                  <td></td>
+                  <td colspan="3">
+                    <p style="margin-bottom:8px;">
+                      <i class="fa fa-minus" @click="rmSeasoningIrdnt(index)" style="color:red;"></i>
+                      &nbsp;{{ seasoningirdnt.seasoningirdnt_nm }}&nbsp;{{ seasoningirdnt.seasoningirdnt_vol }}
+                    </p>
                   </td>
                 </tr>
                 <tr>
@@ -61,7 +106,7 @@
                     <h6>레시피</h6>
                     <h6>개요</h6>
                   </th>
-                  <td>
+                  <td colspan="3">
                     <b-form-textarea
                       v-model="recipedc"
                       id="textarea-small"
@@ -74,15 +119,15 @@
               </table>
             </div>
 
-            <div class="container">
+            <div class="container selectbox">
               <table>
               <tbody>
                 <tr>
                   <th>
-                    <h6>카테고리</h6>
+                    <h6 style="margin-bottom: 8px;">카테고리</h6>
                   </th>
                   <th>
-                    <h6>종류</h6>
+                    <h6 style="margin-bottom: 8px;">종류</h6>
                   </th>
                 </tr>
                 <tr class="shortbox">
@@ -95,10 +140,10 @@
                 </tr>
                 <tr>
                   <th>
-                    <h6>소요 시간(분)</h6>
+                    <h6 style="margin-bottom: 8px;">소요 시간(분)</h6>
                   </th>
                   <th>
-                    <h6>칼로리(kcal)</h6>
+                    <h6 style="margin-bottom: 8px;">칼로리(kcal)</h6>
                   </th>
                 </tr>
                 <tr>
@@ -111,10 +156,10 @@
                 </tr>
                 <tr>
                   <th>
-                    <h6>인분(명)</h6>
+                    <h6 style="margin-bottom: 8px;">인분(명)</h6>
                   </th>
                   <th>
-                    <h6>난이도 : {{ level.text }}</h6>
+                    <h6 style="margin-bottom: 8px;">난이도 : {{ level.text }}</h6>
                   </th>
                 </tr>
                 <tr>
@@ -177,7 +222,7 @@
 		</b-card>
 
     <div class="user">
-			<button class="button btn" @click="checkHandler()" style="width:180px; margin-bottom:30px; background-color:rgb(241, 196, 15);">
+			<button class="button btn" @click="checkHandler()" style="width:190px; margin-bottom:30px; background-color:rgb(241, 196, 15);">
         <i class="fa fa-check"></i>&nbsp;레시피 등록 완료
       </button>
 		</div>
@@ -200,14 +245,21 @@ export default {
       //form 정보
       recipenm: '',
       mainirdnt: '',
+      mainirdnt_amount: '',
       subirdnt: '',
+      subirdnt_amount: '',
       seasoningirdnt: '',
+      seasoningirdnt_amount: '',
       recipedc: '',
-      selectedcat: '',
-      selectedty: '',
+      selectedcat: null,
+      selectedty: null,
       time : '',
       kcal : '',
       person : '',
+
+      arr_mainirdnt: [],
+      arr_subirdnt: [],
+      arr_seasoningirdnt: [],
 
       //select 박스 정보
       irdnts: [],
@@ -234,6 +286,10 @@ export default {
         { value: '퓨전', text: '퓨전' },
         { value: '기타', text: '기타' },
 		  ],
+      units: [
+        { value: null, text: '단위' },
+        { value: 'g', text: 'g' },
+		  ],
 		  level: { value: '2', text: '보통'},
     }
   },
@@ -249,6 +305,57 @@ export default {
 		});
 	},
 	methods: {
+    addMainIrdnt() {
+      let err = true;
+      let msg = '';
+
+      !this.mainirdnt && (msg = '주재료를 입력해주세요', err = false);
+      err && !this.mainirdnt_amount && ((msg = '주재료 양을 입력해주세요'), (err = false));
+
+      if (!err) alert(msg);
+			else {
+        this.arr_mainirdnt.push({mainirdnt_nm:this.mainirdnt, mainirdnt_vol:this.mainirdnt_amount});
+        this.mainirdnt = "";
+        this.mainirdnt_amount = "";
+      }
+    },
+    rmMainIrdnt(index) {
+      this.arr_mainirdnt.splice(index, 1);
+    },
+    addSubIrdnt() {
+      let err = true;
+      let msg = '';
+
+      !this.subirdnt && (msg = '부재료를 입력해주세요', err = false);
+      err && !this.subirdnt_amount && ((msg = '부재료 양을 입력해주세요'), (err = false));
+
+      if (!err) alert(msg);
+			else {
+        this.arr_subirdnt.push({subirdnt_nm:this.subirdnt, subirdnt_vol:this.subirdnt_amount});
+        this.subirdnt = "";
+        this.subirdnt_amount = "";
+      }
+    },
+    rmSubIrdnt(index) {
+      this.arr_subirdnt.splice(index, 1);
+    },
+    addSeasoningIrdnt() {
+      let err = true;
+      let msg = '';
+
+      !this.seasoningirdnt && (msg = '양념을 입력해주세요', err = false);
+      err && !this.seasoningirdnt_amount && ((msg = '양념 양을 입력해주세요'), (err = false));
+
+      if (!err) alert(msg);
+			else {
+        this.arr_seasoningirdnt.push({seasoningirdnt_nm:this.seasoningirdnt, seasoningirdnt_vol:this.seasoningirdnt_amount});
+        this.seasoningirdnt = "";
+        this.seasoningirdnt_amount = "";
+      }
+    },
+    rmSeasoningIrdnt(index) {
+      this.arr_seasoningirdnt.splice(index, 1);
+    },
     flevel: function() {
 			if(this.level.value == '1') {
 				this.level.text = '초보환영';
@@ -271,13 +378,13 @@ export default {
       let msg = '';
       
       for(var i = 0; i < this.steps.length; i++) {
-				!this.steps[i].cookingDc  && (msg = '레시피 상세 설명을 입력해주세요', err = false);
+				!this.steps[i].cookingDc  && (msg = '레시피 과정 정보를 입력해주세요', err = false);
       }
       
 			!this.recipenm && (msg = '레시피 이름을 입력해주세요', err = false);
-			err && !this.mainirdnt && ((msg = '주재료를 입력해주세요'), (err = false));
-			err && !this.subirdnt && ((msg = '부재료를 입력해주세요'), (err = false));
-			err && !this.seasoningirdnt && ((msg = '양념 재료를 입력해주세요'), (err = false));
+			err && this.arr_mainirdnt.length == 0 && ((msg = '주재료를 입력해주세요'), (err = false));
+			err && this.arr_subirdnt.length == 0 && ((msg = '부재료를 입력해주세요'), (err = false));
+			err && this.arr_seasoningirdnt.length == 0 && ((msg = '양념 재료를 입력해주세요'), (err = false));
 			err && !this.recipedc && ((msg = '레시피 설명을 입력해주세요'), (err = false));
 			err && !this.selectedcat && ((msg = '카테고리를 선택해주세요'), (err = false));
       err && !this.selectedty && ((msg = '종류를 선택해주세요'), (err = false));
@@ -287,54 +394,54 @@ export default {
 				this.saveGibonHandler();
 			}
     },
-    saveGibonHandler(){
+    saveGibonHandler() {
       http
-				.post("/recipes/add/recipe",{ // 레시피 기본 정보 등록
-					recipe: {
-						recipeNmKo: this.recipenm,
-						sumry: this.recipedc,
-						nationNm: this.selectedcat,
-						tyNm: this.selectedty,
-						cookingTime: this.time,
-						calorie: this.kcal,
-						qnt: this.person,
-						levelNm: this.level.text
-					},
-					irdnts:[{
-						irdntNm: this.mainirdnt,
-						irdntTyNm: '주재료'
-					},
-					{
-						irdntNm: this.subirdnt,
-						irdntTyNm: '부재료'
-					},
-					{
-						irdntNm: this.seasoningirdnt,
-						irdntTyNm: '양념'
-					}]
-				})
-        .then(({ data }) => {
-          this.addRecipeId = data;
-					this.saveCookingHandler(this.addRecipeId);
-				})
-				.catch((error) => {
-						console.dir(error);
-				});
-			}
+      .post("/recipes/add/recipe",{ // 레시피 기본 정보 등록
+        recipe: {
+          recipeNmKo: this.recipenm,
+          sumry: this.recipedc,
+          nationNm: this.selectedcat,
+          tyNm: this.selectedty,
+          cookingTime: this.time,
+          calorie: this.kcal,
+          qnt: this.person,
+          levelNm: this.level.text
+        },
+        irdnts:[{
+          irdntNm: this.mainirdnt,
+          irdntTyNm: '주재료'
+        },
+        {
+          irdntNm: this.subirdnt,
+          irdntTyNm: '부재료'
+        },
+        {
+          irdntNm: this.seasoningirdnt,
+          irdntTyNm: '양념'
+        }]
+      })
+      .then(({ data }) => {
+        this.addRecipeId = data;
+        this.saveCookingHandler(this.addRecipeId);
+      })
+      .catch((error) => {
+          console.dir(error);
+      });
     },
     saveCookingHandler(recipeId) {
       http
-			.post("/recipes/add/cookings",{ // 레시피 상세 정보 등록
-				recipeId: recipeId,
-				cookings: this.steps
-			})
-			.then(({ data }) => {
-				alert("레시피가 등록 완료되었습니다.");
-				this.$router.push('myrecipe');
-			})
-			.catch((error) => {
-					console.dir(error);
-		});
+      .post("/recipes/add/cookings",{ // 레시피 상세 정보 등록
+        recipeId: recipeId,
+        cookings: this.steps
+      })
+      .then(({ data }) => {
+        alert("레시피가 등록 완료되었습니다.");
+        this.$router.push('myrecipe');
+      })
+      .catch((error) => {
+          console.dir(error);
+      });
+    }
   }
 }
 </script>
@@ -360,7 +467,7 @@ h1, h4, h5, h6 {
 	font-weight: bold;
 }
 .container {
-	width: 90%;
+	max-width: 500px;
 	margin-top: 20px;
 }
 th {
