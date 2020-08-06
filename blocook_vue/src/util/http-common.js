e7119c28505544a51d334d7df04f41c2d9de5351
배진영 <wlsdudqo97@gmail.com>
2020-08-06 08:53:19 +0000

    Update http-common.js

--- blocook_vue/src/util/http-common.js
+++ blocook_vue/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // axios 객체를 생성
 export default axios.create({
-  baseURL: 'http://localhost:9999/api',
+  baseURL: 'http://i3a505.p.ssafy.io:9999/api',
   headers: {
     // 'Content-type': 'applicatoin/json',
   },
