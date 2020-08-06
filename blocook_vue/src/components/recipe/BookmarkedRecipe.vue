185470f1c358875f8cf692c9e5fbf8e2de30d169
김소희 <sohi9732@naver.com>
2020-08-06 21:41:09 +0900

    [sohee] : fix addrecipe design

---
+++ blocook_vue/src/components/recipe/BookmarkedRecipe.vue
@@ -0,0 +1,80 @@
+<template>
+	<!-- 좋아요 표시한 레시피 -->
+	<div class="container" style="width: 90%;margin-top:10px;margin-bottom:30px;">
+		<div class="row">
+			<div class="col-6">
+			<!-- <div class="col-6" v-for="myRecipe in myRecipes" :key="myRecipe.recipeId"> -->
+				<div class="card">
+					<div class="foodimg">
+						<img src="@/assets/img/logo.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body" style="padding:5px;">
+						<h5 class="card-title" style="margin-bottom:3px;">좋아요 레시피</h5>
+						<p class="card-text">레시피 설명</p>
+						<div>
+							<!-- <router-link :to="" class="btn" style="background-color:#B3D662;">레시피&nbsp;<img style="width:25px; height: 25px; margin-top:0px;" src="@/assets/img/recipeicon.png"></router-link> -->
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-6">
+			<!-- <div class="col-6" v-for="myRecipe in myRecipes" :key="myRecipe.recipeId"> -->
+				<div class="card">
+					<div class="foodimg">
+						<img src="@/assets/img/logo.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body" style="padding:5px;">
+						<h5 class="card-title" style="margin-bottom:3px;">좋아요 레시피</h5>
+						<p class="card-text">레시피 설명</p>
+						<div>
+							<!-- <router-link :to="" class="btn" style="background-color:#B3D662;">레시피&nbsp;<img style="width:25px; height: 25px; margin-top:0px;" src="@/assets/img/recipeicon.png"></router-link> -->
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-6">
+			<!-- <div class="col-6" v-for="myRecipe in myRecipes" :key="myRecipe.recipeId"> -->
+				<div class="card">
+					<div class="foodimg">
+						<img src="@/assets/img/logo.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body" style="padding:5px;">
+						<h5 class="card-title" style="margin-bottom:3px;">좋아요 레시피</h5>
+						<p class="card-text">레시피 설명</p>
+						<div>
+							<!-- <router-link :to="" class="btn" style="background-color:#B3D662;">레시피&nbsp;<img style="width:25px; height: 25px; margin-top:0px;" src="@/assets/img/recipeicon.png"></router-link> -->
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-6">
+			<!-- <div class="col-6" v-for="myRecipe in myRecipes" :key="myRecipe.recipeId"> -->
+				<div class="card">
+					<div class="foodimg">
+						<img src="@/assets/img/logo.png" class="card-img-top" alt="...">
+					</div>
+					<div class="card-body" style="padding:5px;">
+						<h5 class="card-title" style="margin-bottom:3px;">좋아요 레시피</h5>
+						<p class="card-text">레시피 설명</p>
+						<div>
+							<!-- <router-link :to="" class="btn" style="background-color:#B3D662;">레시피&nbsp;<img style="width:25px; height: 25px; margin-top:0px;" src="@/assets/img/recipeicon.png"></router-link> -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- blocook_vue/src/views/user/Mypage.vue
+++ blocook_vue/src/views/user/Mypage.vue
@@ -36,36 +36,25 @@
                 </div>
                 
                 <!-- 좋아요 표시한 레시피 -->
-                <div class="container" style="width: 90%;margin-top:10px;" v-if="like">
-                  <div class="row">
-                    <div class="col-6">
-                    <!-- <div class="col-6" v-for="myRecipe in myRecipes" :key="myRecipe.recipeId"> -->
-                      <div class="card">
-                        <div class="foodimg">
-                          <img src="@/assets/img/logo.png" class="card-img-top" alt="...">
-                        </div>
-                        <div class="card-body" style="padding:5px;">
-                          <h5 class="card-title" style="margin-bottom:3px;">좋아요 레시피</h5>
-                          <p class="card-text">레시피 설명</p>
-                          <div>
-                            <!-- <router-link :to="" class="btn" style="background-color:#B3D662;">레시피&nbsp;<img style="width:25px; height: 25px; margin-top:0px;" src="@/assets/img/recipeicon.png"></router-link> -->
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                <div class="col-lg-12">
+                  <BookmarkedRecipe v-if="like == true"/>
                 </div>
             </div>
         </div>
     </div>
 </div>
 </template>
+
 <script>
+import BookmarkedRecipe from '@/components/recipe/BookmarkedRecipe.vue'
 
 export default {
+  components: {
+    BookmarkedRecipe,
+  },
   data() {
     return {
-      title: "login page",
+      title: "My page",
       like: false,
     };
   },
@@ -81,59 +70,4 @@ export default {
     }
 	},
 };
-
 </script>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
-
-button {
-	margin-bottom: 20px;
-}
-.btn {
-	width: 98%;
-}
-.col-6 {
-	margin-bottom: 10px;
-}
-img {
-	margin-top: 10px;
-	width: 100px;
-	height: 100px;
-}
-.card {
-	height: 306px;
-}
-.card-body {
-	height: 200px;
-	/* display: inline-block;   */
-	/* // inline-block으로 설정 필요 */
-}
-.card-title {
-  height: 30px;
-  background-image: url("../../assets/img/highlight.png");
-	background-size: 85%;
-	background-repeat: no-repeat;
-}
-.card-text {
-	text-align: left;
-	height: 80px;
-	width: 120px;
-	margin-bottom: 10px;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	display: -webkit-box;
-    -webkit-line-clamp: 3; /* 라인수 */
-    -webkit-box-orient: vertical;
-    word-wrap:break-word; 
-}
-.foodimg img {
-	margin-top: 10px;
-	width: 120px;
-	height: 120px;
-}
-.recipebook {
-	width:40px;
-	height: 43px;
-}
-</style>
