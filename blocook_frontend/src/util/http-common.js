c0c41581eaf4661d54a881d706a49272608e78b7
DESKTOP-KVCQHCD\multicampus <wlsdudqo97@gmail.com>
2020-08-17 12:50:30 +0900

    [jinyoung] : Change port number

--- blocook_frontend/src/util/http-common.js
+++ blocook_frontend/src/util/http-common.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // axios 객체를 생성
 export default axios.create({
-  //baseURL: 'https://i3a505.p.ssafy.io:9999/api',
-  baseURL: 'http://localhost:9999/api',
+  baseURL: 'https://i3a505.p.ssafy.io:8888/api',
+  // baseURL: 'https://localhost:8888/api',
   headers: {
     // 'Content-type': 'applicatoin/json',
   },
